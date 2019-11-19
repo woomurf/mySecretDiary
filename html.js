f481012655692aa24c6c046c17b95e51317fc623
woomurf <dngusdnd@gmail.com>
2019-11-19 09:55:25 +0900

    make create and update and delete

--- html.js
+++ html.js
@@ -1,6 +1,11 @@
+var formidable = require('formidable');
+var fs = require('fs');
+
 module.exports = {
-    HTML : function(year,month,datelist,imgslider,article,cssfile,swipefile){
-        
+    HTML : function(year,month,datelist,imgslider,article,id){
+        var cssfile = fs.readFileSync(`./src/home.css`,'utf-8');
+        var swipefile = fs.readFileSync(`./src/swipe.js`,`utf-8`);
+
         var html  = `
             <!DOCTYPE html>
                 <html>
@@ -15,7 +20,7 @@ module.exports = {
                         </script>
                         <div id = "main_date">
                             <h1 id = "year">${year}년 </h1>
-                            <h2 id = "month">${month}월 </h2>
+                            ${month}
                         </div>
                         <div id = "who">
                             <p>이름 의 비밀 다이어리</p>
@@ -38,17 +43,22 @@ module.exports = {
                             </div>
                             <div id = "article-wrap">
                                 ${article}
-                            </div>
-    
-                            
+                            </div>                        
                         </section>
+                        <a href="/create">create</a> 
+                        <a href="/update?id=${id}">update</a>
+                        <form action="/delete_process" method = "post">
+                            <input type = "hidden" name = "id" value = "${id}">
+                            <input type = "submit" value = "delete">
+                        </form>
+                        
+                         
                     </body>
                 </html>`;
         return html;
 
         
     },
-
     datelist : function(articles){
         var datelist = "";
 
@@ -57,6 +67,11 @@ module.exports = {
         });
 
         return datelist;
+    },
+    monthlist : function (months) {
+        var monthlist = `<select name = "month">`;
+
+        
     },
 
     imgslider : function(imagelist){
@@ -77,6 +92,94 @@ module.exports = {
         content += `<div id = "content"> <p>${article.content}</p> </div>`;
 
         return content;
+    },
+
+    createArticle : function () {
+        var html = `
+        <!doctype html>
+        <html>
+            <head>
+                <title> 나만의 비밀 다이어리 글쓰기 </title>
+                <meta charset = "utf-8">
+                <div id = "who">
+                    <p>이름 의 비밀 다이어리</p>
+                </div>
+            </head>
+            <body>
+                <form action = "/create_process" method = "post">
+                    <div id = "setDate">
+                        <input type = "date" name = "day">
+                    </div>
+                    <div id = "articleUpload">
+                        <p> 
+                            <input type = "text" name = "title">
+                        </p>
+                        <p>
+                            <textarea name = "content"></textarea>
+                        </p>
+                    </div>
+                    <input type = "submit">
+                </form>
+            </body>
+        </html>
+        `;
+
+        return html;
+    },
+    imgHTML : function (id) {
+        var html = `
+        <!doctype html>
+        <html>
+        <head>
+            <meta charset = "utf-8">
+        </head>
+        <body>
+            <form action = "/image_process" method ="post" enctype = "multipart/form-data">
+
+                <div id = "imageUpload">
+                    <input type="file" name = "images">
+                    <input type = "file" value = "${id}" name = "id">
+                </div>
+                <input type = "submit" value = "업로드">
+            </form>
+        </body>
+        </html>`;
+        return html;
+    },
+    updateArticle : function (title,content,year,month,date) {
+        var html = `
+        <!doctype html>
+        <html>
+            <head>
+                <title> 나만의 비밀 다이어리 글쓰기 </title>
+                <meta charset = "utf-8">
+                <div id = "who">
+                    <p>이름 의 비밀 다이어리</p>
+                </div>
+            </head>
+            <body>
+                <form action = "/create_process" method = "post">
+                    <div id = "setDate">
+                        <input type = "date" name = "day" value = "${year}-${month}-${date}">
+                    </div>
+                    <div id = "imageUpload">
+                        <input type="file" name = "images">
+                        <input type = "submit">
+                    </div>
+                    <div id = "articleUpload">
+                        <p> 
+                            <input type = "text" name = "title" value = "${title}">
+                        </p>
+                        <p>
+                            <textarea name = "content" >${content}</textarea>
+                        </p>
+                    </div>
+                </form>
+            </body>
+        </html>
+        `;
+
+        return html;
     }
 }
 
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var qs = require('querystring');
 var url = require('url');
 var template = require('./html.js');
 var fs = require('fs');
+var formidable = require('formidable');
+var path = require('path');
 
 var db = mysql.createConnection({
     host : 'localhost',
@@ -17,10 +19,14 @@ var app = http.createServer( function(request, response){
     var reqUrl = request.url;
     var queryData = url.parse(reqUrl, true).query;
     var pathname = url.parse(reqUrl, true).pathname;
+    // var filepath = path.join(process.cwd()+"/",'nothing.png');
+    // var stat = fs.statSync(filepath);
+    // var readStream = fs.createReadStream(filepath);
+    // readStream.pipe(response);
 
     if(pathname === '/'){
         if(queryData.id === undefined){
-            db.query(`SELECT * FROM article`,function(error,articles){
+            db.query(`SELECT * FROM article ORDER BY year,month,date`,function(error,articles){
                 if (error){
                     throw error;
                 }
@@ -38,44 +44,32 @@ var app = http.createServer( function(request, response){
                         throw error2;
                     }
                     
+
                     if (images.length == 0){
-                        var default_img = ["./nothing.png"];
+                        var default_img = [{image_link : `/image/nothing.png`}];
                         imgslider = template.imgslider(default_img);
                     }
                     else{
                         imgslider = template.imgslider(images);
                     }
-                    
-                    fs.readFile(`./src/home.css`,function (error1,css) {
-                        if(error1){
-                            throw error1;
-                        }
-                        fs.readFile(`./src/swipe.js`,function (error2,swipe) {
-                            if(error2){
-                                throw error2;
-                            }
+
+                    var html = template.HTML(year,month,datelist,imgslider,content,id);
                             
-                            var html = template.HTML(year,month,datelist,imgslider,content,css.toString(),swipe.toString());
-                            
-                            response.writeHead(200);
-                            response.end(html);
-                            
-                        });
-                    });
+                    response.writeHead(200);
+                    response.end(html);
                 });
                 
             });
         }
         else{
-            db.query(`SELECT * FROM article`,function (error1,articles) {
+            db.query(`SELECT * FROM article ORDER BY year,month,date`,function (error1,articles) {
                 if(error1){
                     throw error1;
                 }
 
-                db.query(`SELECT * FROM article WHERE id = ?`,[queryData.id], function e(error2,article) {
+                db.query(`SELECT * FROM article WHERE id = ? ORDER BY year,month,date `,[queryData.id], function e(error2,article) {
                     
                     var id    = article[0].id;
-                    var title = article[0].title;
                     var year  = article[0].year;
                     var month = article[0].month;
 
@@ -89,8 +83,11 @@ var app = http.createServer( function(request, response){
                         }
                         
                         if (images.length == 0){
-                            var default_img = ["./nothing.png"];
-                            imgslider = template.imgslider(default_img);
+                            fs.readFile('./nothing.png',function (img) {
+                                var default_img = [{image_link : "./nothing.png"}];
+                                imgslider = template.imgslider(default_img);
+                            });
+                            
                         }
                         else{
                             imgslider = template.imgslider(images);
@@ -105,19 +102,120 @@ var app = http.createServer( function(request, response){
                                     throw error2;
                                 }
                                 
-                                var html = template.HTML(year,month,datelist,imgslider,content,css.toString(),swipe.toString());
+                                var html = template.HTML(year,month,datelist,imgslider,content,css.toString(),swipe.toString(),id);
                                 
                                 response.writeHead(200);
                                 response.end(html);
                                 
                             });
                         });    
-                    });
-
-                    
+                    });   
                 });
             });
         }
+    }
+    
+    else if (pathname === '/create'){
+        db.query(`SELECT * FROM article`,function (error,articles) {
+            if(error){
+                throw error;
+            }
+
+            var createHTML = template.createArticle();
+            console.log(createHTML);
+            
+            response.writeHead(200);
+            response.end(createHTML);
+
+        })
+    }
+    else if(pathname ==='/image_process'){
+        var body = '';
+        request.on('data',function (data) {
+            body += data;
+        });
+        request.on('end',function () {
+            var post = qs.parse(body);
+            var id = post.id;
+            console.log(id);
+            
+        })
+        var form = new formidable.IncomingForm();
+        form.encoding = "utf-8";
+
+        form.parse(request,function (err,fields,files) {
+            var oldpath = files.images.path;
+            var newpath = files.images.name;
+            var id = files.id.name;
+            console.log(id);
+            
+            fs.rename(oldpath,newpath,function (err) {
+            });
+
+            //db.query(`INSERT INTO images(image_link,article_id) VALUSE(?,?)`,[])
+        })
+        
+    }
+    else if(pathname ==='/create_process'){
+        var body = '';
+        request.on('data',function (data) {
+            body += data;
+        });
+        request.on('end',function () {
+            var post = qs.parse(body);
+            
+            var day = post.day;
+            console.log(day);
+            
+            var date = day.split('-');
+            var title = post.title;
+            var content = post.content;
+
+            db.query(`INSERT INTO article(title,content,year,month,date) VALUES(?,?,?,?,?)`,[title,content,date[0],date[1],date[2]],function (error,result) {
+                
+                var id = result.insertId;
+                var html = template.imgHTML(id);
+                response.writeHead(200);
+                response.end(html);
+            })
+        })
+    }
+    else if( pathname === '/delete_process'){
+        var body = '';
+        request.on('data',function (data) {
+            body += data;
+        });
+        request.on('end',function () {
+            var post = qs.parse(body);
+            var id = post.id;
+
+            db.query(`DELETE FROM article WHERE id = ?`,[id],function (error) {
+                if (error){
+                    throw error;
+                }
+
+                response.writeHead(302,{Location: `/`});
+                response.end();
+            })
+        })
+    }
+    else if(pathname === '/update'){
+        db.query(`SELECT * FROM article WHERE id = ?`,[queryData.id],function (error,article) {
+            if (error){
+                throw error;
+            }
+
+            var title = article[0].title;
+            var content = article[0].content;
+            var year = article[0].year;
+            var month = article[0].month;
+            var date = article[0].date;
+
+            var updatehtml = template.updateArticle(title,content,year,month,date);
+            response.writeHead(200);
+            response.end(updatehtml);
+
+        });
     }
     else {
         response.writeHead(404);
